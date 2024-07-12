a4d48b44ce17dfe0946a8592cf5c0638e281ce71
Vladimir <alfa.voland@mail.ru>
2024-07-12 14:58:19 +0500

    added calendar logic on the departure screen

--- airTickets/src/main/java/com/alfadroid/departure/presentation/DepartureFragment.kt
+++ airTickets/src/main/java/com/alfadroid/departure/presentation/DepartureFragment.kt
@@ -1,5 +1,6 @@
 package com.alfadroid.departure.presentation
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,18 +8,31 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.alfadroid.airtickets.databinding.FragmentDepartureBinding
 import com.alfadroid.destination.presentation.DestinationBottomSheetFragment
+import java.util.Calendar
 
 class DepartureFragment : Fragment() {
 
     private lateinit var binding: FragmentDepartureBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentDepartureBinding.inflate(layoutInflater)
+
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day_month = calendar.get(Calendar.DAY_OF_MONTH)
+
+        updateDateDayInViews(year, month, day_month)
+
+        binding.llDepartureDate.setOnClickListener {
+            showDatePickedDialog(year, month, day_month)
+        }
+
         return binding.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -40,5 +54,52 @@ class DepartureFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         DestinationBottomSheetFragment().show(parentFragmentManager, "BottomSheetFragment")
+    }
+
+    private fun showDatePickedDialog(year: Int, month: Int, dayMonth: Int) {
+        val datePickerDialog = DatePickerDialog(
+            requireContext(), { _, selectedYear, selectedMonth, selectedDay ->
+                updateDateDayInViews(selectedYear, selectedMonth, selectedDay)
+            }, year, month, dayMonth
+        )
+        datePickerDialog.show()
+    }
+
+    private fun updateDateDayInViews(year: Int = 0, month: Int = 0, day: Int = 0) {
+        binding.tvDepartureDate.text = String.format("%02d %s", day, getMonthString(month + 1))
+        binding.tvDepartureDay.text = String.format(", %s", getDayOfWeekString(year, month, day))
+    }
+
+    private fun getMonthString(month: Int): String {
+        return when (month) {
+            1 -> getString(com.alfadroid.common.R.string.january)
+            2 -> getString(com.alfadroid.common.R.string.february)
+            3 -> getString(com.alfadroid.common.R.string.march)
+            4 -> getString(com.alfadroid.common.R.string.april)
+            5 -> getString(com.alfadroid.common.R.string.may)
+            6 -> getString(com.alfadroid.common.R.string.june)
+            7 -> getString(com.alfadroid.common.R.string.july)
+            8 -> getString(com.alfadroid.common.R.string.august)
+            9 -> getString(com.alfadroid.common.R.string.september)
+            10 -> getString(com.alfadroid.common.R.string.october)
+            11 -> getString(com.alfadroid.common.R.string.november)
+            12 -> getString(com.alfadroid.common.R.string.december)
+            else -> ""
+        }
+    }
+
+    private fun getDayOfWeekString(year: Int, month: Int, day: Int): String {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, day)
+        return when (calendar.get(Calendar.DAY_OF_WEEK)) {
+            Calendar.MONDAY -> getString(com.alfadroid.common.R.string.monday)
+            Calendar.SUNDAY -> getString(com.alfadroid.common.R.string.tuesday)
+            Calendar.TUESDAY -> getString(com.alfadroid.common.R.string.wednesday)
+            Calendar.WEDNESDAY -> getString(com.alfadroid.common.R.string.thursday)
+            Calendar.THURSDAY -> getString(com.alfadroid.common.R.string.friday)
+            Calendar.FRIDAY -> getString(com.alfadroid.common.R.string.saturday)
+            Calendar.SATURDAY -> getString(com.alfadroid.common.R.string.sunday)
+            else -> ""
+        }
     }
 }
